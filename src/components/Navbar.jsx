1acbb0854debf2e1caf8249d3541cb1187cffce6
tahiee <niazi.tahirkhan15@gmail.com>
2024-01-07 02:51:57 +0500

    add style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 function NavScrollExample() {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
     const toggleMobileMenu = () => {
         setMobileMenuOpen(!isMobileMenuOpen);
     };
+
     return (
         <>
             <nav className="your-navbar-class flex justify-between items-center text-center h-auto z-200 text-2xl leading-10 px-14 py-9 w-full top-0 left-0 nav-font">
-                <h3 className='hover:text-emerald-400 cursor-pointer'>Tahir.dev</h3>
+                <h3 className='hover:text-emerald-400 cursor-pointer'  onClick={() => scroll.scrollToTop({ smooth: true})} duration={200} spy={true} offset={20} >Tahir.dev</h3>
                 <ul className="gap-4 flex justify-between items-center text-center ">
-                    <li className="hidden lg:block hover:text-lime-600"><a href="#">Home</a></li>
+                    <li className="hidden lg:block hover:text-lime-600" ><a href="#" >Home</a></li>
+                    {/* <Link className="hidden lg:block hover:text-lime-400" to="about" smooth={true} duration={400} spy={true} offset={20}>
+                        About
+                    </Link> */}
                     <li className="hidden lg:block hover:text-lime-400"><a href="#">About</a></li>
                     <li className="hidden lg:block hover:text-lime-300"><a href="#">Projects</a></li>
                     <li className="hidden lg:block hover:text-lime-300"><a href="#">Contact</a></li>
@@ -33,8 +38,8 @@ function NavScrollExample() {
                 </span>
                 <ul>
                     <li><a href="#home">Home</a></li>
-                    <li><a href="#about">Projects</a></li>
                     <li><a href="#projects">About</a></li>
+                    <li><a href="#about">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
