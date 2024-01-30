79ce91e91fd7dc887d93c46500a3078856c09830
tahiee <niazi.tahirkhan15@gmail.com>
2024-01-30 15:26:46 +0500

    navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { animateScroll as scroll } from 'react-scroll';
 import './App.css'
@@ -27,22 +27,48 @@ function NavScrollExample() {
         return navigate('/about');
     }
 
+    const [scrolling, setScrolling] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolling(true);
+            } else {
+                setScrolling(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <>
-            <nav className="your-navbar-class flex justify-between items-center text-center h-auto z-200 text-2xl leading-10 px-14 py-9 w-full top-0 left-0 nav-font">
+            <nav className={`your-navbar-class flex justify-between items-center text-center h-auto z-200 text-2xl leading-10 px-14 py-9 w-full top-0 left-0 nav-font  ${isMobileMenuOpen ? 'bg-emerald-200 text-black' : ''} ${scrolling ? 'bg-emerald-200 text-black' : ''
+                }`} >
                 <h3 className='hover:text-emerald-400 cursor-pointer' onClick={() => scroll.scrollToTop({ smooth: true })} duration={200} spy={true} offset={20}>Tahir.dev</h3>
-        
+
                 <ul className="gap-4 flex justify-between items-center text-center ">
-                    <li className="hidden lg:block hover:text-lime-600" onClick={gotoHome}>Home</li>
-                    <li className="hidden lg:block hover:text-lime-400" onClick={gotoAbout}>About</li>
-                    <li className="hidden lg:block hover:text-lime-300" onClick={gotoProjects}>Projects</li>
-                    <li className="hidden lg:block hover:text-lime-300" onClick={gotoContact}>Contact</li>
+                    <li className="hidden lg:block cursor-pointer hover:text-emerald-400" onClick={() => { toggleMobileMenu(); gotoHome(); }}>Home</li>
+                    <li className="hidden lg:block cursor-pointer hover:text-emerald-400" onClick={() => { toggleMobileMenu(); gotoAbout(); }}>About</li>
+                    <li className="hidden lg:block cursor-pointer hover:text-emerald-400" onClick={() => { toggleMobileMenu(); gotoProjects(); }}>Projects</li>
+                    <li className="hidden lg:block cursor-pointer hover:text-emerald-400" onClick={() => { toggleMobileMenu(); gotoContact(); }}>Contact</li>
                     <li className="lg:hidden" onClick={toggleMobileMenu}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="mobile-menu">
-                            <path d="M4 6l16 0"></path>
-                            <path d="M4 12l16 0"></path>
-                            <path d="M4 18l16 0"></path>
-                        </svg>
+                        {isMobileMenuOpen ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="tabler-icon tabler-icon-x">
+                                <path d="M18 6l-12 12"></path>
+                                <path d="M6 6l12 12"></path>
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mobile-menu">
+                                <path d="M4 6l16 0"></path>
+                                <path d="M4 12l16 0"></path>
+                                <path d="M4 18l16 0"></path>
+                            </svg>
+                        )}
+
                     </li>
                 </ul>
             </nav>
