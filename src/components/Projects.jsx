398cc9ac4a8fbfaf2524e8d2efcfc7b0e26d4f16
tahiee <niazi.tahirkhan15@gmail.com>
2024-05-11 01:04:00 +0500

    Update project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,12 @@ import "slick-carousel/slick/slick-theme.css";
 import img1 from "../img/Projects/todo-img/todo1.png";
 import img2 from "../img/Projects/todo-img/todo2.png";
 import img3 from "../img/Projects/todo-img/todo3.png";
+import Ecimg1 from "../img/Projects/todo-img/ecommer1.png";
+import Ecimg2 from "../img/Projects/todo-img/ecommer2.png";
+import Ecimg3 from "../img/Projects/todo-img/ecommer3.png";
+import Ecimg4 from "../img/Projects/todo-img/ecommer4.png";
+import Ecimg5 from "../img/Projects/todo-img/ecommer5.png";
+import Ecimg6 from "../img/Projects/todo-img/ecommer6.png";
 import "./App.css";
 
 const Projects = () => {
@@ -22,62 +28,135 @@ const Projects = () => {
       <div className="containers">
         <h2 className="new-projects"> Projects</h2>
       </div>
-      <div className="flex justify-center items-center text-center mt-[12px]">
-        <div className="project-card">
-          <div className="project-info">
-            <h3 className="text-2xl font-bold mb-4 text-[22px] mt-[5px]">
-              TodoApp
-            </h3>
-            <p className="text-lg mb-4">
-              A simple Todo App built using React - Material-UI - Firebase
-            </p>
-          </div>
-          <div className="flex justify-center items-center text-center mt-[2px]">
-            <Slider className="" {...sliderSettings}>
-              <div>
-                <img className="" src={img1} alt="Image 1" />
-              </div>
-              <div>
-                <img src={img2} alt="Image 2" />
-              </div>
-              <div>
-                <img src={img3} alt="Image 2" />
-              </div>
+      <div className="main-parent">
+        {/* my first Project */}
+        <div className="flex justify-center items-center text-center mt-[12px]">
+          <div className="project-card">
+            <div className="project-info">
+              <h3 className="text-2xl font-bold mb-4 text-[22px] mt-[5px]">
+                Nest Mart & Grocery Store
+              </h3>
+              <p className="text-lg mb-4">E-commerce Website</p>
+            </div>
+            <div className="flex justify-center items-center text-center mt-[2px]">
+              <Slider className="" {...sliderSettings}>
+                <div>
+                  <img className="" src={Ecimg2} alt="Ecommerce1" />
+                </div>
+                <div>
+                  <img src={Ecimg1} alt="Ecommerce2" />
+                </div>
+                <div>
+                  <img src={Ecimg3} alt="Ecommerce3" />
+                </div>
+                <div>
+                  <img src={Ecimg4} alt="Ecommerce3" />
+                </div>
+                <div>
+                  <img src={Ecimg5} alt="Ecommerce3" />
+                </div>
+                <div>
+                  <img src={Ecimg6} alt="Ecommerce3" />
+                </div>
 
-              {/* Add more slides as needed */}
-            </Slider>
+                {/* Add more slides as needed */}
+              </Slider>
+            </div>
+            {/* View Project Link */}
+            <div>
+              <button
+                className="button-85"
+                style={{
+                  marginTop: "20px",
+                }}
+              >
+                <a
+                  href="https://nestmartgrocery.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="no-underline text-[15px] text-blue-500 underline hover:text-blue-700"
+                >
+                  View App
+                </a>
+              </button>
+              <button
+                className="button-85"
+                style={{
+                  marginLeft: "10px",
+                }}
+              >
+                <a
+                  href="https://github.com/tahiee/Nest-Mart-Grocery-Reast.js-"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="no-underline text-[15px] text-blue-500 underline hover:text-blue-700"
+                >
+                  GitHub
+                </a>
+              </button>
+            </div>
           </div>
-          {/* View Project Link */}
-          <div>
-            <button className="button-85"
-              style={{
-                marginTop: "20px",
-              }}
-            >
-              <a
-                href="https://todoapp-react-lovat.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="no-underline text-[15px] text-blue-500 underline hover:text-blue-700"
+        </div>
+
+        {/* my sencond project */}
+        <div className="flex justify-center items-center text-center mt-[12px]">
+          <div className="project-card">
+            <div className="project-info">
+              <h3 className="text-2xl font-bold mb-4 text-[22px] mt-[5px]">
+                TodoApp
+              </h3>
+              <p className="text-lg mb-4">
+                A simple Todo App built using React - Material-UI - Firebase
+              </p>
+            </div>
+            <div className="flex justify-center items-center text-center mt-[2px]">
+              <Slider className="" {...sliderSettings}>
+                <div>
+                  <img className="" src={img1} alt="Image 1" />
+                </div>
+                <div>
+                  <img src={img2} alt="Image 2" />
+                </div>
+                <div>
+                  <img src={img3} alt="Image 2" />
+                </div>
+
+                {/* Add more slides as needed */}
+              </Slider>
+            </div>
+            {/* View Project Link */}
+            <div>
+              <button
+                className="button-85"
+                style={{
+                  marginTop: "20px",
+                }}
               >
-                View App
-              </a>
-            </button>
-            <button className="button-85"
-              style={{
-                marginLeft:"10px"
-              }}
-            >
-              <a
-                href="https://github.com/tahiee/TodoApp-React-Firbase"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="no-underline text-[15px] text-blue-500 underline hover:text-blue-700"
+                <a
+                  href="https://todoapp-react-lovat.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="no-underline text-[15px] text-blue-500 underline hover:text-blue-700"
+                >
+                  View App
+                </a>
+              </button>
+              <button
+                className="button-85"
+                style={{
+                  marginLeft: "10px",
+                }}
               >
-                GitHub
-              </a>
-            </button>
-            
+                <a
+                  href="https://github.com/tahiee/TodoApp-React-Firbase"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="no-underline text-[15px] text-blue-500 underline hover:text-blue-700"
+                >
+                  GitHub
+                </a>
+              </button>
+            </div>
           </div>
         </div>
       </div>
