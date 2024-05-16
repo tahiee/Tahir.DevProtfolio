c315b84175a0d2e8c69333fe45df3c735090882f
tahiee <niazi.tahirkhan15@gmail.com>
2024-05-16 14:58:08 +0500

    Cursor Ani

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
-import { useState } from "react";
 import htmlicon from "../img/html-icon.webp";
 import cssicon from "../img/css.png";
 import jsicon from "../img/js.png";
@@ -9,16 +8,203 @@ import tahirimg from "../img/hero-banner-tahir.png";
 import hand from "../img/png-transparent-hand-waving-wave-emoji-wave-hand-arm-sign-thumbnail.png";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
-import Footer from "../components/Footer";
 import About from "./About";
-import LikeButton from "./LikeButton";
+import gsap from "gsap";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
+  const largeHeaderRef = useRef(null);
+  const canvasRef = useRef(null);
+  let width,
+    height,
+    ctx,
+    points,
+    target,
+    animateHeader = true;
+
+  useEffect(() => {
+    initHeader();
+    initAnimation();
+    addListeners();
+    return () => {
+      // Clean up event listeners if needed
+    };
+  }, []);
+
+  function initHeader() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    target = { x: width / 2, y: height / 2 };
+
+    largeHeaderRef.current.style.height = height + "px";
+
+    ctx = canvasRef.current.getContext("2d");
+    canvasRef.current.width = width;
+    canvasRef.current.height = height;
+
+    // create points
+    points = [];
+    for (let x = 0; x < width; x = x + width / 20) {
+      for (let y = 0; y < height; y = y + height / 20) {
+        const px = x + (Math.random() * width) / 20;
+        const py = y + (Math.random() * height) / 20;
+        const p = { x: px, originX: px, y: py, originY: py };
+        points.push(p);
+      }
+    }
+
+    // for each point find the 5 closest points
+    for (let i = 0; i < points.length; i++) {
+      const closest = [];
+      const p1 = points[i];
+      for (let j = 0; j < points.length; j++) {
+        const p2 = points[j];
+        if (!(p1 === p2)) {
+          let placed = false;
+          for (let k = 0; k < 5; k++) {
+            if (!placed) {
+              if (closest[k] === undefined) {
+                closest[k] = p2;
+                placed = true;
+              }
+            }
+          }
+
+          for (let k = 0; k < 5; k++) {
+            if (!placed) {
+              if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                closest[k] = p2;
+                placed = true;
+              }
+            }
+          }
+        }
+      }
+      p1.closest = closest;
+    }
+
+    // assign a circle to each point
+    for (let i in points) {
+      const c = new Circle(
+        points[i],
+        2 + Math.random() * 2,
+        "rgba(255,255,255,0.3)"
+      );
+      points[i].circle = c;
+    }
+  }
+
+  function addListeners() {
+    if (!("ontouchstart" in window)) {
+      window.addEventListener("mousemove", mouseMove);
+    }
+    window.addEventListener("scroll", scrollCheck);
+    window.addEventListener("resize", resize);
+  }
+
+  function mouseMove(e) {
+    const posx =
+      e.pageX ||
+      e.clientX +
+        document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+    const posy =
+      e.pageY ||
+      e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    target.x = posx;
+    target.y = posy;
+  }
+
+  function scrollCheck() {
+    animateHeader = document.body.scrollTop <= height;
+  }
+
+  function resize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    largeHeaderRef.current.style.height = height + "px";
+    canvasRef.current.width = width;
+    canvasRef.current.height = height;
+  }
+
+  function initAnimation() {
+    animate();
+    for (let i in points) {
+      shiftPoint(points[i]);
+    }
+  }
+
+  function animate() {
+    if (animateHeader) {
+      ctx.clearRect(0, 0, width, height);
+      for (let i in points) {
+        // detect points in range
+        if (Math.abs(getDistance(target, points[i])) < 4000) {
+          points[i].active = 0.3;
+          points[i].circle.active = 0.6;
+        } else if (Math.abs(getDistance(target, points[i])) < 20000) {
+          points[i].active = 0.1;
+          points[i].circle.active = 0.3;
+        } else if (Math.abs(getDistance(target, points[i])) < 40000) {
+          points[i].active = 0.02;
+          points[i].circle.active = 0.1;
+        } else {
+          points[i].active = 0;
+          points[i].circle.active = 0;
+        }
+
+        drawLines(points[i]);
+        points[i].circle.draw();
+      }
+    }
+    requestAnimationFrame(animate);
+  }
+
+  function shiftPoint(p) {
+    gsap.to(p, 1 + 1 * Math.random(), {
+      x: p.originX - 50 + Math.random() * 100,
+      y: p.originY - 50 + Math.random() * 100,
+      ease: "circ.inOut",
+      onComplete: () => {
+        shiftPoint(p);
+      },
+    });
+  }
+
+  function drawLines(p) {
+    if (!p.active) return;
+    for (let i in p.closest) {
+      ctx.beginPath();
+      ctx.moveTo(p.x, p.y);
+      ctx.lineTo(p.closest[i].x, p.closest[i].y);
+      ctx.strokeStyle = "rgba(156,217,249," + p.active + ")";
+      ctx.stroke();
+    }
+  }
+
+  function Circle(pos, rad, color) {
+    const _this = this;
+    _this.pos = pos || null;
+    _this.radius = rad || null;
+    _this.color = color || null;
+
+    _this.draw = function () {
+      if (!_this.active) return;
+      ctx.beginPath();
+      ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+      ctx.fillStyle = "rgba(156,217,249," + _this.active + ")";
+      ctx.fill();
+    };
+  }
+
+  function getDistance(p1, p2) {
+    return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+  }
 
   return (
     <>
-      <section id="home" className="hero">
+      <section id="home" className="hero" ref={largeHeaderRef}>
+        <canvas id="demo-canvas" ref={canvasRef}></canvas>
+        <div className="cursor"></div>
         <div className="containers">
           <div className="content">
             <div className="hero-main">
@@ -111,7 +297,6 @@ const Home = () => {
       <About />
       <Projects />
       <Contact />
-      {/* <Footer /> */}
     </>
   );
 };
